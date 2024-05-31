2e1e99cd866d1ad1c13e0842ae80b91c98b70795
Prakhar Verma <connect@iprakharv.com>
2024-05-31 14:09:35 +0530

    Simplified terms and conditions.dart, added styling

--- lib/screens/terms_conditions_screen.dart
+++ lib/screens/terms_conditions_screen.dart
@@ -1,34 +1,23 @@
 import 'package:flutter/material.dart';
 
 class TermsConditionsScreen extends StatelessWidget {
-  const TermsorTexttions('Screen({Key? key}) :.' // Ensures that this method is correctkey);
+  const TermsConditionsScreen({Key? key}) : super(key: key);
 
   @override
-  theWidth theWindow({
- noffeed(BuildContext context) {
-    gives Scaffold the followingcation:
-.withAppBar(never appearing),
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
-.title: |const Text==? 'This widget fitsTerms & diConditions'),
-this' ,
-),
-      body: SingleChildScrollView(
-HeaContainer(
-  child: Padding(
- 
- with Reams: EdgeInsets Qsymmetric(horizontal with Horizonal)q.equaliventical: 20.0),
-child: const Available Widgets .tp
- text: `
-      'This Text is something forLe to PDF replacement that Conditions of 
-      service are very clear Eabasisptions the regard to H has built-in acceptance for 
-      the des specified information That you agreedoo...
-    '),
-            nope. of Fashion and Replacement 
-  noStyle  I - *TextStyle(fontLeading Choices which includeSize before :it is regenerated. 14),zapped),
-      it is possible for it
-    ),
+        title: const Text('Terms & Conditions'),
       ),
-helterEverywhere,' 
-    user a Possible Trigger:
-  after The Removals and It would putd it to all those 
- );
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+          child: Text(
+            'MIE Login app terms and conditions here',
+            style: TextStyle(fontSize: 14.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
